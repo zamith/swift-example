56bd6f22a751003fcbbc3b7718de8ac92b78b792
Zamith <zamith.28@gmail.com>
2014-12-09 23:55:03 +0000

    Finished class 13

--- Example App/ViewController.swift
+++ Example App/ViewController.swift
@@ -9,15 +9,18 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var myLabel: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        println("Hello World!")
+        
+        myLabel.text = "It works!"
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
 
 
